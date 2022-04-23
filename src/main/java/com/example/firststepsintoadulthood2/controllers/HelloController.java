efb9428beefc200dd351ad639af2cf6f9ef0413b
malinapavel <malina.pavel@student.upt.ro>
2022-04-23 14:25:52 +0300

    Create register page GUI.

--- src/main/java/com/example/firststepsintoadulthood2/controllers/HelloController.java
+++ src/main/java/com/example/firststepsintoadulthood2/controllers/HelloController.java
@@ -1,4 +1,4 @@
-package com.example.firststepsintoadulthood2;
+package com.example.firststepsintoadulthood2.controllers;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
@@ -9,6 +9,6 @@ public class HelloController {
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        welcomeText.setText("Account successfully created.");
     }
 }
