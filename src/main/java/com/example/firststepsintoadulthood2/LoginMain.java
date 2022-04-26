9a766b18e71d18a6bce8ace739c8a79500961c77
valentinnistoran <valentin.nistoran@student.upt.ro>
2022-04-27 00:20:27 +0300

    Login Page Created

---
+++ src/main/java/com/example/firststepsintoadulthood2/LoginMain.java
@@ -0,0 +1,27 @@
+package com.example.firststepsintoadulthood2;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class LoginMain extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("login.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 800, 700);
+        stage.setTitle("First Steps Into Adulthood - Login Page");
+        Image img = new Image("icon.png");
+        stage.getIcons().add(img);
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ src/main/java/com/example/firststepsintoadulthood2/controllers/LoginController.java
@@ -0,0 +1,12 @@
+package com.example.firststepsintoadulthood2.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class LoginController {
+
+    @FXML
+    protected void LoginButtonClick() {
+
+    }
+}
