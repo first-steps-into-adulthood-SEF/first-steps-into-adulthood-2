9edfffc886add04bbc9b5c0b0a54087cac861a05
valentinnistoran <valentin.nistoran@student.upt.ro>
2022-05-04 21:32:25 +0300

    Page for viewing page, return to forum button and properties setters

--- src/main/java/com/example/firststepsintoadulthood2/controllers/LoginController.java
+++ src/main/java/com/example/firststepsintoadulthood2/controllers/LoginController.java
@@ -77,15 +77,6 @@ public class LoginController {
     @FXML
     public VBox verticalBox;
 
-//    public EventHandler<ActionEvent> openPost(Post post) throws IOException {
-//        Parent root = FXMLLoader.load(Main.class.getResource("postPage.fxml"));
-//        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-//        Scene scene = new Scene(root);
-//        stage.setScene(scene);
-//        stage.show();
-//        return null;
-//    }
-
     @FXML
     public void initialize() {
 
