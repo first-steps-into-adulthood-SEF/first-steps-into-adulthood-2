fd33db2036a35e9a59a87c9ed46ae6575dc60170
valentinnistoran <valentin.nistoran@student.upt.ro>
2022-05-06 14:35:54 +0300

    commited a controller

---
+++ src/main/java/com/example/firststepsintoadulthood2/controllers/UserReportController.java
@@ -0,0 +1,24 @@
+package com.example.firststepsintoadulthood2.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class UserReportController {
+
+    public TextField reportDescription;
+    public Label reportedUserUsername;
+    public Button sendReportButton;
+    public Button closeReportFormButton;
+    public TextField stolenIdentityUsername;
+
+    public void sendUserReport(){
+
+    }
+
+    public void closeReportForm(ActionEvent actionEvent) {
+    }
+}
