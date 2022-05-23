9c365facf58a717eb861bcb63c265c91ad03521a
malinapavel <malina.pavel@student.upt.ro>
2022-05-23 12:06:00 +0300

    Create post deletion mechanism

--- src/main/java/com/example/firststepsintoadulthood2/controllers/ForumController.java
+++ src/main/java/com/example/firststepsintoadulthood2/controllers/ForumController.java
@@ -1,10 +1,12 @@
 package com.example.firststepsintoadulthood2.controllers;
 
 import com.example.firststepsintoadulthood2.Main;
+import com.example.firststepsintoadulthood2.exceptions.CouldNotWritePostsException;
 import com.example.firststepsintoadulthood2.model.Post;
 import com.example.firststepsintoadulthood2.model.User;
 import com.example.firststepsintoadulthood2.services.PostService;
 import com.example.firststepsintoadulthood2.services.UserService;
+import com.fasterxml.jackson.core.*;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -16,17 +18,26 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
+import org.json.JSONObject;
 
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.Objects;
 import java.util.Stack;
 
 
 
 public class ForumController extends LoginController{
+
+    @FXML
+    public TextField reportedPostTitle;
+    @FXML
+    public TextField reportedPostAuthor;
 
 
     public void switchToInboxPage(ActionEvent event) throws IOException {
@@ -66,6 +77,22 @@ public class ForumController extends LoginController{
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+
+    }
+
+
+    public void proceedToDeletePost() throws IOException, CouldNotWritePostsException {
+
+        UserService.loadUsersFromFile();
+        PostService.loadPostsFromFile();
+
+        User user = UserService.getUser(reportedPostAuthor.getText());
+        String userPost = user.getUsername();
+
+        PostService.deletePostPocedure(userPost, reportedPostTitle.getText());
+
+        //post.removeIf(p -> p.getTitle().equals(reportedPostTitle.getText()) && p.getUsername().equals(Objects.requireNonNull(user).getUsername()));
+
 
     }
 
