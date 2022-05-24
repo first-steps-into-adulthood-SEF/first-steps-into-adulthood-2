9d9312f9c4ba6c7372261598819cb718f4c35b7d
malinapavel <malina.pavel@student.upt.ro>
2022-05-24 19:52:48 +0300

    Test registration

---
+++ src/main/java/com/example/firststepsintoadulthood2/Launcher.java
@@ -0,0 +1,7 @@
+package com.example.firststepsintoadulthood2;
+
+public class Launcher {
+    public static void main(String[] args) {
+        LoginMain.main(args);
+    }
+}
--- src/main/java/com/example/firststepsintoadulthood2/services/FileSystemService.java
+++ src/main/java/com/example/firststepsintoadulthood2/services/FileSystemService.java
@@ -9,13 +9,19 @@ import java.nio.file.Paths;
 
 public class FileSystemService {
 
-    private static final String APPLICATION_FOLDER = "folder_json";
+    public static String APPLICATION_FOLDER = "folder_json";
     private static final String USER_FOLDER = "src/main/resources";
     public static final Path APPLICATION_HOME_PATH = Paths.get(USER_FOLDER, APPLICATION_FOLDER);
 
     public static Path getPathToFile(String... path) {
 
         return APPLICATION_HOME_PATH.resolve(Paths.get("", path));
+
+    }
+
+    public static Path getApplicationHomePath() {
+
+        return Paths.get(USER_FOLDER, APPLICATION_FOLDER);
 
     }
 
